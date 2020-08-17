0022aabd675789d32b7356d25fc115c0cb835689
yuhongwen <15071469547@163.com>
2020-08-17 16:34:04 +0800

    add dataBinging

---
+++ databinging/src/main/java/com/application/databinging/DataBingingActivityDemo.java
@@ -0,0 +1,13 @@
+package com.application.databinging;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DataBingingActivityDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_binging_demo);
+    }
+}
